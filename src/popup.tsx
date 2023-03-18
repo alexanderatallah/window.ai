3947acd873fcca5d2aadb999e88605cd2970d4c7
Alex Atallah <alexander.atallah@gmail.com>
2023-03-18 01:36:06 -0700

    add button for stripe

--- src/popup.tsx
+++ src/popup.tsx
@@ -6,6 +6,29 @@ const EmailShowcase = () => {
   return (
     <div>
       Your email is: <b>{userInfo?.email}</b>
+      <button
+        disabled={!userInfo}
+        onClick={async () => {
+          chrome.identity.getAuthToken(
+            {
+              interactive: true
+            },
+            (token) => {
+              if (!!token) {
+                window.open(
+                  `${
+                    process.env.PLASMO_PUBLIC_STRIPE_LINK
+                  }?client_reference_id=${
+                    userInfo.id
+                  }&prefilled_email=${encodeURIComponent(userInfo.email)}`,
+                  "_blank"
+                )
+              }
+            }
+          )
+        }}>
+        Unlock GPT-4 with Stripe
+      </button>
     </div>
   )
 }
@@ -19,9 +42,7 @@ function IndexPopup() {
           flexDirection: "column",
           padding: 16
         }}>
-        <h1>
-          Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-        </h1>
+        <h1>Welcome to LLMixer!</h1>
         <EmailShowcase />
       </div>
     </UserInfoProvider>
