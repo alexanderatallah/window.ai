db1fae2d5ece767255bcd86e23839973afd1a8ee
Alex Atallah <alexander.atallah@gmail.com>
2023-03-24 15:39:28 -0400

    tailwind install and basic popup ui

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,5 +1,9 @@
 import useSWR from "swr"
 
+import "./style.css"
+
+import { useState } from "react"
+
 import { get, post } from "~core/api"
 import type { CompletionResponse } from "~core/constants"
 import { UserInfoProvider, useUserInfo } from "~core/user-info"
@@ -72,21 +76,50 @@ const PremiumFeatureButton = () => {
   )
 }
 
-function IndexPopup() {
+function Popup() {
+  const [query, setQuery] = useState("")
+  const shouldConfigure = !useUserInfo()
   return (
     <UserInfoProvider>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          padding: 16
-        }}>
-        <h1>Bring your own LLM</h1>
-        <EmailShowcase />
-        <PremiumFeatureButton />
+      <div className="bg-gray-900 p-0 text-gray-400 leading-6 font-sans">
+        <input
+          // onKeyDown={this.keyDown}
+          value={query}
+          onInput={(e) => setQuery(e.currentTarget.value)}
+          className="w-64 p-4 pl-10 bg-transparent border-none outline-none text-gray-400 rounded-sm"></input>
+        <div className="w-80 min-h-48">
+          <EmailShowcase />
+          <PremiumFeatureButton />
+          <div
+            className={`p-2 grid grid-cols-7 gap-2 cursor-pointer ${
+              // ix === selectedIx ? "bg-gray-800" : ""
+              ""
+            } hover:bg-gray-850`}
+            onClick={() => this.selectTransform(transform)}>
+            <div className="emoji">🧠</div>
+            <div className="col-span-6 overflow-hidden">
+              <div className="overflow-hidden truncate">OpenAI</div>
+              <div className="overflow-hidden truncate text-xs text-gray-600">
+                Set up GPT-4
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className={`p-2 ${
+            shouldConfigure ? "text-gray-400 font-bold" : "text-gray-500"
+          }`}>
+          <a
+            href="#"
+            onClick={this.goToOptionsPage}
+            className="block p-2 text-center rounded-sm hover:bg-gray-850">
+            {shouldConfigure && "Required - add OpenAI API key"}
+            {!shouldConfigure && "Add transform"}
+          </a>
+        </div>
       </div>
     </UserInfoProvider>
   )
 }
 
-export default IndexPopup
+export default Popup
