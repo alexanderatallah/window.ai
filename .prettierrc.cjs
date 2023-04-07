3ff81793a464434e314285a3dca623f81ce56f68
Colin Fortuner <cfortuner@gmail.com>
2023-04-07 12:08:18 -0400

    adding prettier and eslint to the root

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import('prettier').Options}
+ */
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: false,
+  trailingComma: "none",
+  bracketSpacing: true,
+  bracketSameLine: true
+}
--- apps/web/components/Chat.tsx
+++ apps/web/components/Chat.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useRef, useState } from "react";
-import { Button } from "./Button";
-import { type ChatGPTMessage, ChatLine, LoadingChatLine } from "./ChatLine";
-import { useCookies } from "react-cookie";
-import { DISCORD_URL, DOWNLOAD_URL } from "./common";
+import { useEffect, useRef, useState } from "react"
+import { Button } from "./Button"
+import { type ChatGPTMessage, ChatLine, LoadingChatLine } from "./ChatLine"
+import { useCookies } from "react-cookie"
+import { DISCORD_URL, DOWNLOAD_URL } from "./common"
 
-const COOKIE_NAME = "nextjs-example-ai-chat-gpt3";
+const COOKIE_NAME = "nextjs-example-ai-chat-gpt3"
 
 // default first message to display in UI (not necessary to define the prompt)
 export const initialMessages: ChatGPTMessage[] = [
   {
     role: "assistant",
-    content: "Hi! I am a friendly AI assistant. Ask me anything!",
-  },
-];
+    content: "Hi! I am a friendly AI assistant. Ask me anything!"
+  }
+]
 
 const InputMessage = ({ input, setInput, sendMessage }: any) => (
   <div className="mt-6 flex clear-both">
@@ -24,124 +24,123 @@ const InputMessage = ({ input, setInput, sendMessage }: any) => (
       value={input}
       onKeyDown={(e) => {
         if (e.key === "Enter") {
-          sendMessage(input);
-          setInput("");
+          sendMessage(input)
+          setInput("")
         }
       }}
       onChange={(e) => {
-        setInput(e.target.value);
+        setInput(e.target.value)
       }}
     />
     <Button
       type="submit"
       className="ml-4 flex-none"
       onClick={() => {
-        sendMessage(input);
-        setInput("");
-      }}
-    >
+        sendMessage(input)
+        setInput("")
+      }}>
       Say
     </Button>
   </div>
-);
+)
 
 export function Chat() {
-  const [messages, setMessages] = useState<ChatGPTMessage[]>(initialMessages);
-  const [input, setInput] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [cookie, setCookie] = useCookies([COOKIE_NAME]);
-  const [showInstallMessage, setShowInstallMessage] = useState(false);
-  const [permissionDenied, setPermissionDenied] = useState(false);
+  const [messages, setMessages] = useState<ChatGPTMessage[]>(initialMessages)
+  const [input, setInput] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [cookie, setCookie] = useCookies([COOKIE_NAME])
+  const [showInstallMessage, setShowInstallMessage] = useState(false)
+  const [permissionDenied, setPermissionDenied] = useState(false)
 
   useEffect(() => {
     if (!cookie[COOKIE_NAME]) {
       // generate a semi random short id
-      const randomId = Math.random().toString(36).substring(7);
-      setCookie(COOKIE_NAME, randomId);
+      const randomId = Math.random().toString(36).substring(7)
+      setCookie(COOKIE_NAME, randomId)
     }
-  }, [cookie, setCookie]);
+  }, [cookie, setCookie])
 
   // send message to API /api/chat endpoint
   const sendMessage = async (message: string) => {
     if (!("ai" in window)) {
-      setShowInstallMessage(true);
-      return;
+      setShowInstallMessage(true)
+      return
     }
-    setShowInstallMessage(false);
-    setLoading(true);
+    setShowInstallMessage(false)
+    setLoading(true)
     const newMessages = [
       ...messages,
-      { role: "user", content: message } as ChatGPTMessage,
-    ];
-    setMessages(newMessages);
-    const last10messages = newMessages.slice(-10); // remember last 10 messages
+      { role: "user", content: message } as ChatGPTMessage
+    ]
+    setMessages(newMessages)
+    const last10messages = newMessages.slice(-10) // remember last 10 messages
 
-    const responseMsg = { role: "assistant", content: "" };
-    const allMsgs = [...newMessages];
+    const responseMsg = { role: "assistant", content: "" }
+    const allMsgs = [...newMessages]
 
     //@ts-ignore
-    setMessages([...allMsgs, { ...responseMsg }]);
-    setPermissionDenied(false);
+    setMessages([...allMsgs, { ...responseMsg }])
+    setPermissionDenied(false)
 
     try {
       await window.ai.getCompletion(
         {
-          messages: [...last10messages],
+          messages: [...last10messages]
         },
         {
           onStreamResult: (result: any, error: any) => {
             if (error) {
-              throw error;
+              throw error
             }
 
-            responseMsg.content += result.message.content;
+            responseMsg.content += result.message.content
 
             //@ts-ignore
-            setMessages([...allMsgs, { ...responseMsg }]);
-          },
+            setMessages([...allMsgs, { ...responseMsg }])
+          }
         }
-      );
+      )
     } catch (e) {
-      console.log("error", e);
+      console.log("error", e)
       if (e === "PERMISSION_DENIED") {
-        setPermissionDenied(true);
+        setPermissionDenied(true)
         setMessages((p) => {
-          p.pop();
-          return [...p];
-        });
-        setLoading(false);
-        return;
+          p.pop()
+          return [...p]
+        })
+        setLoading(false)
+        return
       }
 
       setMessages([
         ...messages,
         {
           role: "assistant",
-          content: "Sorry, I had an error. Please try again later.",
-        },
-      ]);
+          content: "Sorry, I had an error. Please try again later."
+        }
+      ])
 
-      console.log(e);
+      console.log(e)
     }
 
-    setLoading(false);
-  };
+    setLoading(false)
+  }
 
-  const messagesRef = useRef<HTMLDivElement>(null);
+  const messagesRef = useRef<HTMLDivElement>(null)
 
   const scrollToBottom = () => {
     if (messagesRef.current) {
-      messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+      messagesRef.current.scrollTop = messagesRef.current.scrollHeight
     }
-  };
+  }
 
   // scroll when new message is added
 
   useEffect(() => {
     if (messagesRef.current) {
-      scrollToBottom();
+      scrollToBottom()
     }
-  }, [messages]);
+  }, [messages])
 
   return (
     <div className="w-full pt-10">
@@ -167,8 +166,7 @@ export function Chat() {
           <div className="grid grid-cols-2 gap-6">
             <Button
               onClick={() => window.open(DISCORD_URL, "_blank")}
-              className=""
-            >
+              className="">
               Download the beta
             </Button>
             <Button
@@ -177,8 +175,7 @@ export function Chat() {
                   "https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked",
                   "_blank"
                 )
-              }
-            >
+              }>
               How to install
             </Button>
           </div>
@@ -190,5 +187,5 @@ export function Chat() {
         />
       </div>
     </div>
-  );
+  )
 }
