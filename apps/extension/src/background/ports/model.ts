4b83eee274d6c4033afdb1a77bb3b137ed40f1cc
Alex Atallah <alexander.atallah@gmail.com>
2023-04-30 13:25:48 -0400

    move requestAuth to completion port

--- apps/extension/src/background/ports/model.ts
+++ apps/extension/src/background/ports/model.ts
@@ -1,18 +1,10 @@
-import { ErrorCode, type RequestID } from "window.ai"
+import { ErrorCode } from "window.ai"
 
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 
-import {
-  POPUP_HEIGHT,
-  POPUP_WIDTH,
-  type PortRequest,
-  type PortResponse,
-  RequestInterruptType
-} from "~core/constants"
+import { type PortRequest, type PortResponse } from "~core/constants"
 import { PortName } from "~core/constants"
-import { Extension } from "~core/extension"
 import { AuthType, configManager } from "~core/managers/config"
-import type { Transaction } from "~core/managers/transaction"
 import { err, ok } from "~core/utils/result-monad"
 import { log } from "~core/utils/utils"
 
@@ -48,13 +40,6 @@ const handler: PlasmoMessaging.PortHandler<
   res.send({
     id,
     response: ok({ model: configManager.getCurrentModel(config) })
-  })
-}
-
-export async function requestAuth(requestId: RequestID) {
-  await Extension.openPopup(POPUP_WIDTH, POPUP_HEIGHT, {
-    requestInterruptType: RequestInterruptType.Authentication,
-    requestId
   })
 }
 
