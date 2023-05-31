852dc6bd60b3cd6de8385f07cf92f0aa0acdc2ea
Alex Atallah <alexander.atallah@gmail.com>
2023-05-30 22:45:58 -0400

    add port listener destruction to relay

--- apps/extension/src/contents/relay.ts
+++ apps/extension/src/contents/relay.ts
@@ -21,44 +21,16 @@ const portStates: Record<
   [PortName.Permission]: {}
 }
 
-function initPortState(portName: PortName) {
-  log(`Connecting to ${portName} port`)
-  const portState = portStates[portName]
-  const port = Extension.connectToBackground(portName, () =>
-    initPortState(portName)
-  )
-  portState.port = port
-  // TODO do we need to remove the old listener from the old port?
-  portState.listener = (msg) => {
-    if (!("response" in msg)) {
-      // TODO handle invalid requests
-      console.error(`Invalid request`, msg)
-      return
-    }
-    const res = {
-      type: ContentMessageType.Response,
-      portName,
-      id: msg.id,
-      response: msg.response
-    }
-    window.postMessage(res, "*")
-  }
-  // Handle responses from background script
-  Extension.addPortListener<typeof portName, PortResponse>(
-    portState.listener,
-    port
-  )
-}
-
+// Initialize all ports, aside from Permission
 ;(Object.keys(portStates) as PortName[]).forEach((portName) => {
   if (portName === PortName.Permission) {
     // Only used for background script
     return
   }
-  initPortState(portName)
+  _initPortState(portName)
 })
 
-// Listen to all other incoming messages as events
+// Listen to all incoming messages as events
 Extension.addPortListener((msg, port) => {
   if (!("response" in msg)) {
     // TODO handle invalid requests
@@ -71,7 +43,7 @@ Extension.addPortListener((msg, port) => {
     id: null,
     response: msg.response
   }
-  window.postMessage(res, "*")
+  _postMessage(res)
 })
 
 // Handle messages from inpage script
@@ -80,9 +52,12 @@ window.addEventListener("message", (event) => {
 
   // We only accept messages to our window and a port
 
-  const portName = data.portName as PortName
+  const portName = data.portName as PortName | undefined
+  if (source !== window || !portName) {
+    return
+  }
   const port = portStates[portName].port
-  if (source !== window || !port) {
+  if (!port) {
     return
   }
 
@@ -99,3 +74,49 @@ window.addEventListener("message", (event) => {
       break
   }
 })
+
+// Helpers
+
+function _postMessage(msg: {
+  type: ContentMessageType
+  portName: PortName
+  id: string | null
+  response: any
+}) {
+  // log(`Relay sending message: `, msg)
+  window.postMessage(msg, "*")
+}
+
+function _initPortState(portName: PortName) {
+  log(`Connecting to ${portName} port`)
+  const portState = portStates[portName]
+  const port = Extension.connectToBackground(portName, () =>
+    _initPortState(portName)
+  )
+  if (portState.listener && portState.port) {
+    Extension.removePortListener(portState.listener, portState.port)
+    delete portState.port
+    delete portState.listener
+  }
+  portState.port = port
+  // TODO do we need to remove the old listener from the old port?
+  portState.listener = (msg) => {
+    if (!("response" in msg)) {
+      // TODO handle invalid requests
+      console.error(`Invalid request`, msg)
+      return
+    }
+    const res = {
+      type: ContentMessageType.Response,
+      portName,
+      id: msg.id ?? null,
+      response: msg.response
+    }
+    _postMessage(res)
+  }
+  // Handle responses from background script
+  Extension.addPortListener<typeof portName, PortResponse>(
+    portState.listener,
+    port
+  )
+}
